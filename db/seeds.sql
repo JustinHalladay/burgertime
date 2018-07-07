9ef16d4d233796d040e28eb486fbc90ada184d3e
Justin <justaoiabilly99@yahoo.com>
2018-07-07 12:29:29 -0600

    added server.js

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+    values('Veggie Burger', false),
+   ('Bacon Burger', false),
+   ('Blue Burger', false);
+   
+   select * from burgers;
+
+seeds.sql file name
